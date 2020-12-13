218e9c92099292785db4036127c231c5d6ca8983
jackson <jackjack93150@gmail.com>
2020-12-13 16:18:24 +0100

    ajout gravité

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,45 @@
 use ggez::{Context,GameResult,graphics};
+use ggez::graphics::Image;
 use ggez::event::EventHandler;
+use ggez::nalgebra::Vector2;
 mod player;
 use player::Player;
 
 pub struct MyGame {
-    player: Player,
+    runner: Player,
+    player_img : Image,
+    gravity : Vector2<f32>,
 }
 
 impl MyGame {
-    pub fn new(context: &mut Context) -> GameResult<Self> {
-        let player = Player::new(context)?;
+    pub fn new(ctx: &mut Context) -> GameResult<Self> {
+        let runner = Player::new(ctx)?;
+        let player_img = Image::new(ctx,"/perso.png")?;
+        let gravity = Vector2::new(0.0,0.007); // on défini la force de notre gravité
         Ok(MyGame{
-            player,
+            runner,
+            player_img,
+            gravity,
         })
     }
 }
 
 impl EventHandler for MyGame {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        // Update code here...
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+        let (_perimetre_x,perimetre_y) = graphics::drawable_size(ctx); // on recupere la taille de l'écran pour les fixer comme limite
+        self.runner.create_gravity(&self.gravity); // on ajoute nos fonction au player
+        self.runner.setup_gravity();
+        self.runner.limite(perimetre_y);
         Ok(())
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, graphics::Color::from_rgb(53, 10, 49));
-        self.player.draw(ctx);
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, graphics::Color::from_rgb(53, 10, 49)); // couleur du background 
+        graphics::draw(
+            ctx,
+            &self.player_img,
+            graphics::DrawParam::default().dest(self.runner.location()), // on va dessiner notre perso a la position défini
+        )?;
         graphics::present(ctx)
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,26 +1,44 @@
-use ggez::{Context,GameResult,graphics::{self, Image}};
-use ggez::nalgebra;
+use ggez::{Context,GameResult};
+use ggez::nalgebra::Point2;
+use ggez::nalgebra::Vector2;
+
 
 pub struct Player {
-    player_img : Image,
+    position : Vector2<f32>,
+    acceleration : Vector2<f32>,
+    speed : Vector2<f32>,
 }
 
 impl Player{
-    pub fn new(context: &mut Context)->GameResult<Self>{
-        let player_img = Image::new(context,"/perso.png")?;
+    pub fn new(_context: &mut Context)->GameResult<Self>{
+        let position = Vector2::new(150.0,150.0); // initialisation de la position du joueur / acceleration / speed
+        let acceleration = Vector2::new(0.0,0.0);
+        let speed = Vector2::new(0.0,0.0);
         Ok(Player{
-            player_img,
+            position,
+            acceleration,
+            speed,
         })
     }
-    pub fn update(&mut self) -> GameResult{
-        Ok(())
+
+    pub fn location(&self) -> Point2<f32>{ // determine la position du joueur 
+        Point2::new(self.position.x,self.position.y)
     }
-    pub fn draw(&self,context: &mut Context) -> GameResult {
-         graphics::draw(
-            context,
-            &self.player_img,
-            graphics::DrawParam::new().scale(nalgebra::Vector2::new(1.0, 1.0)),
-        )?;
-        Ok(())
+
+    // implementation d'un algorithme simple pour la gravité
+    pub fn create_gravity(&mut self, pression : &Vector2<f32>) {
+        self.acceleration += pression;
+    }
+    pub fn setup_gravity(&mut self) {
+        self.speed += self.acceleration;
+        self.position += self.speed;
+        self.acceleration *= 0.0;
+    }
+    // on fixe des limite pour pas que notre perso sort de l'écran
+    pub fn limite(&mut self,perimetre : f32) {
+        if self.position.y + 75.0 > perimetre {
+            self.position.y = perimetre - 80.0 ; 
+            self.speed.y = 0.0;
+        }
     }
 }

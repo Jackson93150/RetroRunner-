2f57e2f4b25f5d618b3a0fb328af636c199b2c54
jackson <jackjack93150@gmail.com>
2020-12-14 17:06:09 +0100

    ajout deathstar

---
+++ src/deathstar.rs
@@ -0,0 +1,53 @@
+use ggez::{Context,GameResult};
+use ggez::nalgebra::Point2;
+use ggez::nalgebra::Vector2;
+extern crate rand;
+use rand::Rng;
+use super::player::Player;
+
+// on reprend les memes bases que pour le player
+
+pub struct Deathstar{
+    position : Vector2<f32>,
+    movement_left : Vector2<f32>,
+}
+
+
+impl Deathstar{
+    pub fn new(_context: &mut Context)->GameResult<Self>{
+        let y = rand::thread_rng().gen_range(-100.0, 700.0); // on applique une position en y de facon aléatoire
+        let position = Vector2::new(1350.0,y); // et on fixe la position initial de sorte que l'ennemie apparait en dehors de l'écran
+        let movement_left = Vector2::new(-0.25,0.0); // vitesse a laquelle l'ennemie va se deplacer 
+        Ok(Deathstar{
+            position,
+            movement_left,
+        })
+    }
+
+    pub fn location(&self) -> Point2<f32>{ 
+        Point2::new(self.position.x,self.position.y)
+    }
+    pub fn movement(&mut self){ // fonction pour que l'ennemie se deplace continuellement a la gauche
+        self.position += self.movement_left;
+    }
+    pub fn respawn(&mut self){ // fonction qui va permettre de faire réappaitre l'énnemie apres qu'il soit sortie de l'écran
+        let y = rand::thread_rng().gen_range(-100.0, 700.0);
+        if self.position.x < -200.0{
+            self.position = Vector2::new(1350.0,y);
+        }
+    }
+    pub fn speed(&mut self){ // augmente la vitesse de l'énnemmie au fur et a mesure du temps
+        loop{
+            if self.movement_left.x > - 10.0 {
+                self.movement_left.x += - 0.0002;
+            }
+            break;
+        }
+    }
+    pub fn collision(&self,player:&Player){ // fonction qui va déterminer si le joueur est rentrer en collision avec l'énnemie
+        let ploc = player.location(); // (fonction a retravailler pour etres plus précis)
+        if self.position.x >= ploc.x - 30.0 && self.position.x <= ploc.x + 30.0 && self.position.y <= ploc.y + 30.0 && self.position.y >= ploc.y -30.0{
+            println!("me touche pas"); // modifiera cela pour que le resultat arrete le jeu et nous affiche le score + bouton redémarrer
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,14 @@ use ggez::input::keyboard;
 use ggez::input::keyboard::KeyCode;
 mod player;
 use player::Player;
+mod deathstar;
+use deathstar::Deathstar;
 
 pub struct MyGame {
     runner: Player,
     player_img : Image,
+    deathstar : Deathstar,
+    deathstar_img : Image,
     gravity : Vector2<f32>,
 }
 
@@ -17,10 +21,14 @@ impl MyGame {
     pub fn new(ctx: &mut Context) -> GameResult<Self> {
         let runner = Player::new(ctx)?;
         let player_img = Image::new(ctx,"/perso.png")?;
+        let deathstar = Deathstar::new(ctx)?;
+        let deathstar_img = Image::new(ctx,"/ennemie.png")?;
         let gravity = Vector2::new(0.0,0.007); // on défini la force de notre gravité
         Ok(MyGame{
             runner,
             player_img,
+            deathstar,
+            deathstar_img,
             gravity,
         })
     }
@@ -29,12 +37,16 @@ impl MyGame {
 impl EventHandler for MyGame {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
         let (perimetre_x,perimetre_y) = graphics::drawable_size(ctx); // on recupere la taille de l'écran pour les fixer comme limite
-        self.runner.create_gravity(&self.gravity); // on ajoute nos fonction au player
+        self.runner.create_gravity(&self.gravity); // on ajoute nos fonction au player/ennemie 
         self.runner.setup_gravity();
         self.runner.limite(perimetre_y);
         self.runner.limite2(-100.0);
         self.runner.limite3(-20.0); 
         self.runner.limite4(perimetre_x);
+        self.deathstar.movement();
+        self.deathstar.respawn();
+        self.deathstar.speed();
+        self.deathstar.collision(&self.runner);
         if keyboard::is_key_pressed(ctx,KeyCode::Up){ // on assigie nos fonction de déplacement au touche du clavier
             self.runner.fly();
         }
@@ -53,6 +65,11 @@ impl EventHandler for MyGame {
             ctx,
             &self.player_img,
             graphics::DrawParam::default().dest(self.runner.location()), // on va dessiner notre perso a la position défini
+        )?;
+        graphics::draw( // la meme mais pour l'énnemie
+            ctx,
+            &self.deathstar_img,
+            graphics::DrawParam::default().dest(self.deathstar.location()),
         )?;
         graphics::present(ctx)
     }
